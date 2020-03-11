321fad57a54a03ccac80319edaf9c87926aff67d
Jason Sofokleous <jasonsofokleous@Jasons-Macbook-Pro.local>
2020-03-11 16:55:09 +0000

    the test

---
+++ app/controllers/libraries_controller.rb
@@ -0,0 +1,24 @@
+class LibrariesController < ApplicationController
+  def show
+    @assets = Asset.where(library_id: current_library.id)
+  end
+
+  def index
+    @libraries = Library.all
+    render layout: 'library_dashboard'
+  end
+
+  def activity
+    @activity = current_library.activities
+    @activity = current_library.activities.order(created_at: :desc)
+    if request.xhr?
+      render partial: 'shared/activity_list', layout: false
+    end
+  end
+
+  def info
+    if request.xhr?
+      render layout: false
+    end
+  end
+end
